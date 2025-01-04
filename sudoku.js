e522aa70ad69307d4630531415e4d3550254f335
Karan Dugar <krishdugar04@gmail.com>
2025-01-04 06:51:12 +0530

    feat: added game win/lose cases

--- sudoku.js
+++ sudoku.js
@@ -1,5 +1,9 @@
 let selectedCell = 'cell00';
+higlight();
 var id;
+let mistakes = 0;
+let minutes = "00";
+let seconds = "00";
 const sudokuData = {
   "Sudokus": {
     "Sudoku": [
@@ -91,9 +95,14 @@ const sudokuData = {
   }
 };
 
-initialize();
-
 function initialize() {
+  seconds='00';
+  minutes='00';
+  mistakes=0;
+  document.querySelector('.mistakes').innerHTML='Mistakes: '+mistakes+'/3';
+  document.querySelector('.gameover').style.display = 'none';
+  document.querySelector('.gamewon').style.display = 'none';
+  document.querySelector('.overlay').style.display = 'none';
   let cells = document.querySelectorAll('#cell');
   const random = Math.floor(Math.random() * 12);
   const sudoku = sudokuData.Sudokus.Sudoku[random];
@@ -112,16 +121,29 @@ initialize();
 
 function playMove(value){
   const play = document.querySelector(`.${selectedCell}`);
-  if(play.innerHTML === '')
+  if(play.innerHTML === '' || play.style.color == 'red'){
     play.innerHTML = value;
+  }
   const sudoku = sudokuData.Sudokus.Sudoku[id];
   let cells = document.querySelectorAll('#cell');
+  let answer='';
   for (let i = 0; i < cells.length; i++) {
+    answer=answer+cells[i].innerHTML;
     if (sudoku.solution[i] != cells[i].innerHTML && cells[i].innerHTML!=''){
-      play.style.color = "red";
+      mistakes++;
+      play.style.color = 'red';
+      document.querySelector('.mistakes').innerHTML='Mistakes: '+mistakes+'/3';
+      if(mistakes==3){
+        gameover();
+        return;
+      }
     }
+    else
+      cells[i].style.color = "#3d536a";
   }
   higlight();
+  if(answer==sudoku.solution)
+    gameWon();
 }
 
 function selectCell(cellValue){
@@ -205,8 +227,126 @@ function higlight(){
   }
 }
 
+function gameover(){
+  document.querySelector('.gameover').style.display = 'flex';
+  document.querySelector('.overlay').style.display = 'flex';
+}
+
+function secondChance(){
+  mistakes--;
+  document.querySelector('.mistakes').innerHTML='Mistakes: '+mistakes+'/3';
+  document.querySelector('.gameover').style.display = 'none';
+  document.querySelector('.overlay').style.display = 'none';
+}
+
+function  gameWon(){
+  let cells = document.querySelectorAll('#cell');
+  let i = 0;
+  let j = 0;
+  function animation(){
+    if(j%2==0){
+      for(var k=0;k<cells.length;k+=9){
+        cells[i+k].style.backgroundColor = "#00000020";
+      }
+      for(var k=i*9;k<(i*9)+9;k++){
+        cells[k].style.backgroundColor = "#00000020";
+      }
+    }
+    else{
+      for(var k=0;k<cells.length;k+=9){
+        cells[i+k].style.backgroundColor = "#fff";
+      }
+      for(var k=i*9;k<(i*9)+9;k++){
+        cells[k].style.backgroundColor = "#fff";
+      }
+      i++;
+    }
+    j++;
+    if(i<9)
+      setTimeout(animation,35);
+    else
+      return;
+  }
+  setTimeout(animation,35);
+  document.querySelector('.recordTime').innerHTML = minutes + ':' + seconds;
+  document.querySelector('.gamewon').style.display = 'flex';
+  document.querySelector('.overlay').style.display = 'flex';
+}
+
 document.addEventListener("keydown",function(event){
   if(event.key == '1' || event.key == '2' || event.key == '3' || event.key == '4' || event.key == '5' || event.key == '6' || event.key == '7' || event.key == '8' || event.key == '9'){
     playMove(event.key);
   }
+  if(event.key == 'ArrowRight'){
+    let cells = document.querySelectorAll('#cell');
+    let k = 0;
+    for(var i=0;i<cells.length;i++){
+      if(cells[i].className==selectedCell){
+        k=i;
+        break;
+      }
+    }
+    if(k%9==8)
+      selectCell(cells[k-8].className);
+    else
+      selectCell(cells[k+1].className);
+  }
+  if(event.key == 'ArrowLeft'){
+    let cells = document.querySelectorAll('#cell');
+    let k = 0;
+    for(var i=0;i<cells.length;i++){
+      if(cells[i].className==selectedCell){
+        k=i;
+        break;
+      }
+    }
+    if(k%9==0)
+      selectCell(cells[k+8].className);
+    else
+      selectCell(cells[k-1].className);
+  }
+  if(event.key == 'ArrowUp'){
+    let cells = document.querySelectorAll('#cell');
+    let k = 0;
+    for(var i=0;i<cells.length;i++){
+      if(cells[i].className==selectedCell){
+        k=i;
+        break;
+      }
+    }
+    if(k<=8)
+      selectCell(cells[k+72].className);
+    else
+      selectCell(cells[k-9].className);
+  }
+  if(event.key == 'ArrowDown'){
+    let cells = document.querySelectorAll('#cell');
+    let k = 0;
+    for(var i=0;i<cells.length;i++){
+      if(cells[i].className==selectedCell){
+        k=i;
+        break;
+      }
+    }
+    if(k>=72)
+      selectCell(cells[k-72].className);
+    else
+      selectCell(cells[k+9].className);
+  }
 })
+
+setTimeout(timer,1000);
+
+function timer(){
+  seconds = String(parseInt(seconds)+1);
+  if(seconds.length==1)
+    seconds = '0'+seconds;
+  if(seconds=='60'){
+    minutes = String(parseInt(minutes)+1);
+    if(minutes.length==1)
+      minutes = '0'+minutes;
+    seconds = '00';
+  }
+  document.querySelector('.time').innerHTML= minutes + ':' + seconds;
+  setTimeout(timer, 1000);
+}
