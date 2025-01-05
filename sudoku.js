10cff5d1f4db4a543cabcf874c0e9feb08a9d7a6
Karan Dugar <krishdugar04@gmail.com>
2025-01-06 01:38:01 +0530

    added fetch api

--- sudoku.js
+++ sudoku.js
@@ -4,96 +4,24 @@ var id;
 let mistakes = 0;
 let minutes = "00";
 let seconds = "00";
-const sudokuData = {
-  "Sudokus": {
-    "Sudoku": [
-      {
-        "id": "1",
-        "puzzle": "1..5.37..6.3..8.9......98...1.......8761..........6...........7.8.9.76.47...6.312",
-        "solution": "198543726643278591527619843914735268876192435235486179462351987381927654759864312",
-        "clues": "27",
-        "difficulty": "2.2"
-      },
-      {
-        "id": "2",
-        "puzzle": "...81.....2........1.9..7...7..25.934.2............5...975.....563.....4......68.",
-        "solution": "934817256728653419615942738176425893452398167389176542897564321563281974241739685",
-        "clues": "23",
-        "difficulty": "0.0"
-      },
-      {
-        "id": "3",
-        "puzzle": "..5...74.3..6...19.....1..5...7...2.9....58..7..84......3.9...2.9.4.....8.....1.3",
-        "solution": "215983746387654219469271385538716924941325867726849531653198472192437658874562193",
-        "clues": "25",
-        "difficulty": "2.6"
-      },
-      {
-        "id": "4",
-        "puzzle": "........5.2...9....9..2...373..481.....36....58....4...1...358...42.......978...2",
-        "solution": "473816925628539741195427863732948156941365278586172439217693584864251397359784612",
-        "clues": "26",
-        "difficulty": "1.4"
-      },
-      {
-        "id": "5",
-        "puzzle": ".4.1..............653.....1.8.9..74...24..91.......2.8...562....1..7..6...4..1..3",
-        "solution": "947153682128649357653287491381926745572438916496715238839562174215374869764891523",
-        "clues": "25",
-        "difficulty": "1.1"
-      },
-      {
-        "id": "6",
-        "puzzle": "5...634.....7.....1...5.83.....18..7..69......43...9...............7..2.32.64.5..",
-        "solution": "598163472632784159174259836259418367816937245743526981465892713981375624327641598",
-        "clues": "24",
-        "difficulty": "0.0"
-      },
-      {
-        "id": "7",
-        "puzzle": "..346..2..58.2...1.2.9...8...1....9.2..783.........3....9..6..........56.6..7.21.",
-        "solution": "913468527458327961627915483731542698296783145584691372179256834842139756365874219",
-        "clues": "26",
-        "difficulty": "0.0"
-      },
-      {
-        "id": "8",
-        "puzzle": "38.1.........5.6.....9....3.4.........5.18.......9.561.6..2478.8.......6..4.8..2.",
-        "solution": "386172954419853672527946813741365298695218437238497561163524789852739146974681325",
-        "clues": "25",
-        "difficulty": "3.7"
-      },
-      {
-        "id": "9",
-        "puzzle": ".......2...75...9.6....4........7....25.961..9......83...6..85.....1.....4.32..7.",
-        "solution": "894163725137582496652974318481237569325896147976451283713649852268715934549328671",
-        "clues": "23",
-        "difficulty": "0.0"
-      },
-      {
-        "id": "10",
-        "puzzle": ".75.....34......1....672.....9..16.....3....5.2.56...49.7.4......38.9............",
-        "solution": "275184963486935712391672548539421687764398125128567394917243856643859271852716439",
-        "clues": "23",
-        "difficulty": "1.5"
-      },
-      {
-        "id": "11",
-        "puzzle": "..3.8.5...4.....9.7..15.4..98...1..5..2.7983...5.6....1....6.5.8.....7..........4",
-        "solution": "623984517541627398798153426987231645462579831315468972179846253854312769236795184",
-        "clues": "26",
-        "difficulty": "1.5"
-      },
-      {
-        "id": "12",
-        "puzzle": "7.98.5...6..1..8.........9.8..4...1.....5..4.2....7..5..6...789.2.9..........3.6.",
-        "solution": "739845126654192873182376594865439217973251648241687935316524789427968351598713462",
-        "clues": "24",
-        "difficulty": "0.0"
-      }
-    ]
+let sudokuId = Math.floor(Math.random() * 3000000)+1;
+var sudokuData;
+fetchSudokuData(sudokuId);
+async function fetchSudokuData(id) {
+  try {
+    // Make a GET request to the backend API
+    const response = await fetch(`http://localhost:3000/sudoku-data/${id}`);
+    if (!response.ok) {
+      throw new Error('Error fetching data');
+    }
+    const data = await response.json();
+    sudokuData = data.Sudoku;
+    initialize();
+    setTimeout(timer,1000);
+  } catch (error) {
+    console.error('Error:', error);
   }
-};
+}
 
 function initialize() {
   seconds='00';
@@ -104,12 +32,9 @@ function initialize() {
   document.querySelector('.gamewon').style.display = 'none';
   document.querySelector('.overlay').style.display = 'none';
   let cells = document.querySelectorAll('#cell');
-  const random = Math.floor(Math.random() * 12);
-  const sudoku = sudokuData.Sudokus.Sudoku[random];
-  id = random;
   for (let i = 0; i < cells.length; i++) {
-    if (sudoku.puzzle[i] !== '.') {
-      cells[i].innerHTML = sudoku.puzzle[i];
+    if (sudokuData.puzzle[i] !== '.') {
+      cells[i].innerHTML = sudokuData.puzzle[i];
     } else {
       cells[i].innerHTML = ''; 
     }
@@ -117,19 +42,17 @@ function initialize() {
 }
 
 // Call the initialize function to set up the board
-initialize();
 
 function playMove(value){
   const play = document.querySelector(`.${selectedCell}`);
   if(play.innerHTML === '' || play.style.color == 'red'){
     play.innerHTML = value;
   }
-  const sudoku = sudokuData.Sudokus.Sudoku[id];
   let cells = document.querySelectorAll('#cell');
   let answer='';
   for (let i = 0; i < cells.length; i++) {
     answer=answer+cells[i].innerHTML;
-    if (sudoku.solution[i] != cells[i].innerHTML && cells[i].innerHTML!=''){
+    if (sudokuData.solution[i] != cells[i].innerHTML && cells[i].innerHTML!=''){
       mistakes++;
       play.style.color = 'red';
       document.querySelector('.mistakes').innerHTML='Mistakes: '+mistakes+'/3';
@@ -142,7 +65,7 @@ function playMove(value){
       cells[i].style.color = "#3d536a";
   }
   higlight();
-  if(answer==sudoku.solution)
+  if(answer==sudokuData.solution)
     gameWon();
 }
 
@@ -334,8 +257,6 @@ document.addEventListener("keydown",function(event){
       selectCell(cells[k+9].className);
   }
 })
-
-setTimeout(timer,1000);
 
 function timer(){
   seconds = String(parseInt(seconds)+1);
